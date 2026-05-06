211c708d7280aa93f6873f8ccbfbd925dd59e1f2
DeeDeevk <khoa01022020@gmail.com>
2026-05-06 12:53:54 +0700

    set up Eureka and API Gateway for project (Khoa 6/5/2026)

---
+++ hkt-be/api-gateway/src/main/java/fit/iuh/apigateway/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package fit.iuh.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        // 🔥 QUAN TRỌNG: dùng cái này
+        config.setAllowedOriginPatterns(List.of("http://localhost:5173"));
+
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
